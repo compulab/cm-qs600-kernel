ad39a34c9a672dbef4a6075df2204efc167d3d3e
Forest Bond <forest.bond@rapidrollout.com>
2013-10-21 12:38:49 -0400

    HID: hid-multitouch: add support for SiS panels
    Add support for SiS multitouch panels.
    
    Change-Id: Idcc78b190d9faeb4b452e7c386940f0f1b4b399a
    Signed-off-by: Forest Bond <forest.bond@rapidrollout.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -951,6 +951,14 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_QUANTA,
 			USB_DEVICE_ID_QUANTA_OPTICAL_TOUCH_3008) },
 
+	/* SiS panels */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_SIS2_TOUCH,
+		USB_DEVICE_ID_SIS9200_TOUCH) },
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_SIS2_TOUCH,
+		USB_DEVICE_ID_SIS817_TOUCH) },
+
 	/* Stantum panels */
 	{ .driver_data = MT_CLS_CONFIDENCE,
 		HID_USB_DEVICE(USB_VENDOR_ID_STANTUM,
